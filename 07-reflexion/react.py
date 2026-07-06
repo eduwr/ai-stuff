88ae2b123173e04cee1cb4d672806a30c61ceea3
Eduardo Ricardo <eduwric@gmail.com>
2026-07-06 13:31:35 -0300

    Implement structure for code changes tracking

---
+++ 07-reflexion/react.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+def triple(num: float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+
+llm = ChatOpenAI(model="gpt-5.4-mini", temperature=0).bind_tools(tools)
